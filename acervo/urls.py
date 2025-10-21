a5c3b4b5362ea89ae9b31c5274a085f781effbe6
OCarcara <gerencia@ocarcara.online>
2025-10-21 00:11:33 -0300

    Iniciado cadastro das peças. Lista de peças ok e formulário de inserção iniciado. Sem formatação e não testado

--- acervo/urls.py
+++ acervo/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path("", index, name="index")
+    path("pecas", views.pecas_acervo),
+    path("", views.index, name="index"),
+    path("pecas/", views.pecas_acervo, name="pecas_acervo"),
+    path("pecas/add/", views.peca_create, name="pecas_add"),
+    path("pecas/<int:pk>/edit/", views.peca_update, name="pecas_edit"),
+    path("pecas/<int:pk>/delete/", views.peca_delete, name="pecas_delete"),
 ]
 
