d66a931f5d6d0d17a079146540d53d9b293e2c9e
OCarcara <gerencia@ocarcara.online>
2025-10-23 00:05:12 -0300

    Montado layout do formulário de peças até o campo Nº de ordem

--- acervo/urls.py
+++ acervo/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("pecas/", views.pecas_acervo, name="pecas_acervo"),
+    path("pecas/<int:pk>/edit/", views.editar_peca, name="editar_peca"),
+    path("pecas/<int:pk>/atualizar/", views.atualizar_peca, name="atualizar_peca"),
 ]
 """     path("pecas/add/", views.peca_create, name="pecas_add"),
-    path("pecas/<int:pk>/edit/", views.peca_update, name="pecas_edit"),
     path("pecas/<int:pk>/delete/", views.peca_delete, name="pecas_delete"), """
 
--- acervo/views.py
+++ acervo/views.py
@@ -11,6 +11,13 @@ def pecas_acervo(request):
     pecas = PecasAcervo.objects.all().order_by("denominacao")
     return render(request, "pecas/pecas_acervo.html", {"pecas": pecas})
 
+def editar_peca(request, pk):
+    peca = PecasAcervo.objects.get(id=pk)
+    if peca:
+        return render(request, "pecas/peca_form.html", {'peca': peca})
+    pecas_acervo()
 
+def atualizar_peca(request, pk):
+    pecas_acervo()
 
 
